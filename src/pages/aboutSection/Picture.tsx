1f3297c29e245aaebdf823dfcc318018ef52cc38
warkananta96 <warkananta96@gmail.com>
2024-10-09 15:44:05 +0700

    Perfection Picture Component

--- src/pages/aboutSection/Picture.tsx
+++ src/pages/aboutSection/Picture.tsx
@@ -1,14 +1,101 @@
-function Picture() {
+// function Picture() {
+//   return (
+//     <div className="w-[384px] relative">
+//       <img
+//         className="pic w-[364px] h-full absolute flex-shrink-0 top-0 left-0"
+//         src="../../../public/Background.png"
+//         alt=""
+//       />
+//       <div className="pic w-[364px] h-full flex flex-shrink-0 rounded-2xl bg-glass-bg backdrop-blur-5 z-10 border border-solid border-glass"></div>
+//     </div>
+//   );
+// }
+
+// export default Picture;
+
+import React, { useRef } from "react";
+import {
+  motion,
+  useMotionTemplate,
+  useMotionValue,
+  useSpring,
+} from "framer-motion";
+
+const Picture = () => {
   return (
-    <div className="w-[384px] relative">
-      <img
-        className="pic w-[364px] h-full absolute flex-shrink-0 top-0 left-0"
-        src="../../../public/Background.png"
-        alt=""
-      />
-      <div className="pic w-[364px] h-full flex flex-shrink-0 rounded-2xl bg-glass-bg backdrop-blur-5 z-10 border border-solid border-glass"></div>
+    <div className="grid w-[384px] place-content-center">
+      <TiltCard />
     </div>
   );
-}
+};
+
+const ROTATION_RANGE = 32.5;
+const HALF_ROTATION_RANGE = 32.5 / 2;
+
+const TiltCard = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+
+  const xSpring = useSpring(x);
+  const ySpring = useSpring(y);
+
+  const transform = useMotionTemplate`rotateX(${xSpring}deg) rotateY(${ySpring}deg)`;
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (!ref.current) return [0, 0];
+
+    const rect = ref.current.getBoundingClientRect();
+
+    const width = rect.width;
+    const height = rect.height;
+
+    const mouseX = (e.clientX - rect.left) * ROTATION_RANGE;
+    const mouseY = (e.clientY - rect.top) * ROTATION_RANGE;
+
+    const rX = (mouseY / height - HALF_ROTATION_RANGE) * -1;
+    const rY = mouseX / width - HALF_ROTATION_RANGE;
+
+    x.set(rX);
+    y.set(rY);
+  };
+
+  const handleMouseLeave = () => {
+    x.set(0);
+    y.set(0);
+  };
+
+  return (
+    <motion.div
+      ref={ref}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{
+        transformStyle: "preserve-3d",
+        transform,
+        backgroundImage: "url(../../../public/Background.png)",
+      }}
+      className="relative h-96 w-72 rounded-2xl"
+    >
+      <div
+        style={{
+          transform: "translateZ(75px)",
+          transformStyle: "preserve-3d",
+        }}
+        className="absolute inset-4 grid place-content-center rounded-2xl bg-glass-bg backdrop-blur-5 shadow-card"
+      >
+        <p
+          style={{
+            transform: "translateZ(50px)",
+          }}
+          className="text-center text-2xl font-bold text-content"
+        >
+          William Arkananta R.
+        </p>
+      </div>
+    </motion.div>
+  );
+};
 
 export default Picture;
