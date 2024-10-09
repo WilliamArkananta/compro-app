e7393ddcc5c7da74b22f753062cc9b503512545c
warkananta96 <warkananta96@gmail.com>
2024-10-09 15:25:14 +0700

    Adding Picture Component

--- src/pages/aboutSection/Picture.tsx
+++ src/pages/aboutSection/Picture.tsx
@@ -1,12 +1,12 @@
 function Picture() {
   return (
-    <div className="w-[384px] h-[381px] relative">
+    <div className="w-[384px] relative">
       <img
-        className="pic w-[364px] h-[362px] absolute flex-shrink-0 top-0 left-0"
+        className="pic w-[364px] h-full absolute flex-shrink-0 top-0 left-0"
         src="../../../public/Background.png"
         alt=""
       />
-      <div className="pic w-[364px] h-[362px] flex flex-shrink-0 rounded-2xl bg-glass-bg backdrop-blur-5 z-10 border border-solid border-glass"></div>
+      <div className="pic w-[364px] h-full flex flex-shrink-0 rounded-2xl bg-glass-bg backdrop-blur-5 z-10 border border-solid border-glass"></div>
     </div>
   );
 }
